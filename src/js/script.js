7d5185002996aba7ab6e1ad6f400ab3d763ecee8
MahalakshmiPFW <maha.01@pfw.edu>
2025-03-12 14:28:17 -0400

    update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,21 @@ AOS.init({
     duration: 1000,
     offset: 100,
 });
+
+const trendingSushis = [
+    'Make Sushi',
+    'Nigiri Sushi',
+    'Oshizushi',
+    'Temaki Sushi',
+    'Uramaki Sushi',
+    'Inari Sushi',
+];
+
+const trendingDrinks = [
+    'Oruncha',
+    'Ofukucha',
+    'Sakura Tea',
+    'Kombu-cha',
+    'Aojiru',
+    'Mugicha',
+];
